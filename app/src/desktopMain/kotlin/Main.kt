3b378cdc664cbc3802ccea949e4fc56fed946ea5
munch01 <emeric.gaugain@protonmail.comgit config --global user.name>
2026-09-21 20:07:27 +0200

    test playstore test1

--- app/src/desktopMain/kotlin/Main.kt
+++ app/src/desktopMain/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.example.timecraft.App
+import com.emeric.timecraft.App
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "TimeCraft") {
